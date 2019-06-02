cc4afb451b8a5652f955e5ad1a628f395a71f217
SuYuehua <yvette_suyu@163.com>
2019-06-02 10:38:40 +0800

    Update train_net.py

--- ayo/code/train_net.py
+++ ayo/code/train_net.py
@@ -1,28 +1,5 @@
 #!/usr/bin/env python
 # coding=utf-8
-# Copyright 2018 challenger.ai
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-Baseline codes for zero-shot learning task.
-This python script is train a deep feature extractor (CNN).
-The command is:     python train_CNN.py Animals True 0.05
-The first parameter is the super-class.
-The second parameter is the flag whether data preparation is to be implemented. You should choose True at the first running.
-The third parameter is the learning rate of the deep network (MobileNet).
-The trained model will be saved at 'model/mobile_Animals_wgt.h5'
-"""
 
 from __future__ import absolute_import
 from __future__ import division
